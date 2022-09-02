e91791250486282a3cc32fa23b43432479cd3c1f
portableCoder <>
2022-09-02 22:22:13 +0530

    Added husky, eslint & prettier

--- deploy_bot.js
+++ deploy_bot.js
@@ -1,8 +1,9 @@
 const { exec } = require('child_process');
 const { writeFileSync } = require('fs');
 writeFileSync('out/.nojekyll', "")
-exec('yarn publish-page', (err, out) => {
+exec('yarn publish-page-bot', (err, out) => {
     if (err) {
         throw new Error("FAILED TO PUBLISH" + err)
     }
 })
+
